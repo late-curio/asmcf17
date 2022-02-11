153a37806a0ff036028dff5827d73f2c7493c4ab
Todd W Crone <twcrone@gmail.com>
2022-02-11 16:55:21 -0500

    Disable ClassPrinter for now

--- app/src/main/java/asmcf17/agent/ClassPrinter.java
+++ app/src/main/java/asmcf17/agent/ClassPrinter.java
@@ -11,7 +11,7 @@ public class ClassPrinter extends ClassVisitor {
 
     @Override
     public void visit(int version, int access, String name, String signature, String superName, String[] interfaces) {
-        System.out.println(name + " extends " + superName + " {");
+        //System.out.println(name + " extends " + superName + " {");
     }
 
     @Override
@@ -41,18 +41,18 @@ public class ClassPrinter extends ClassVisitor {
 
     @Override
     public FieldVisitor visitField(int access, String name, String descriptor, String signature, Object value) {
-        System.out.println("     " + descriptor + " " + name);
+        //System.out.println("     " + descriptor + " " + name);
         return null;
     }
 
     @Override
     public MethodVisitor visitMethod(int access, String name, String descriptor, String signature, String[] exceptions) {
-        System.out.println("     " + name + descriptor);
+        //System.out.println("     " + name + descriptor);
         return null;
     }
 
     @Override
     public void visitEnd() {
-        System.out.println("}");
+        //System.out.println("}");
     }
 }
