7c37145c9b639be06043c6f70ae1606a155abb27
Todd W Crone <twcrone@gmail.com>
2022-02-11 16:42:54 -0500

    Add separate scripts

--- runWithJava17.sh
+++ runWithJava17.sh
@@ -2,4 +2,5 @@
 . ~/.sdkman/bin/sdkman-init.sh
 sdk use java $JDK8
 ./gradlew clean shadowJar
+sdk use java $JDK17
 java -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005 -javaagent:app/build/libs/app-all.jar -jar app/build/libs/app-all.jar
