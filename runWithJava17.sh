9f55d7528bd69e732c9af77a459655b9cf8c2143
Todd W Crone <twcrone@gmail.com>
2022-02-14 18:01:17 -0500

    Modify to be able to see actual bug once annotations are added

--- runWithJava17.sh
+++ runWithJava17.sh
@@ -3,4 +3,4 @@
 sdk use java $JDK8
 ./gradlew clean shadowJar
 sdk use java $JDK17
-java -Xmx1536M --add-exports=java.base/sun.nio.ch=ALL-UNNAMED --add-opens=java.base/java.lang=ALL-UNNAMED --add-opens=java.base/java.lang.reflect=ALL-UNNAMED --add-opens=java.base/java.io=ALL-UNNAMED --add-exports=jdk.unsupported/sun.misc=ALL-UNNAMED -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005 -javaagent:app/build/libs/app-all.jar -jar app/build/libs/app-all.jar
+java --add-exports=java.base/sun.nio.ch=ALL-UNNAMED --add-opens=java.base/java.lang=ALL-UNNAMED --add-opens=java.base/java.lang.reflect=ALL-UNNAMED --add-opens=java.base/java.io=ALL-UNNAMED --add-exports=jdk.unsupported/sun.misc=ALL-UNNAMED -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005 -javaagent:app/build/libs/app-all.jar -jar app/build/libs/app-all.jar
